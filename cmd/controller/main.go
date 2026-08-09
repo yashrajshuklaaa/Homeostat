e2c782517537a315216c58ce0cefb004120326d3
Yashraj Shukla <shuklayashraj68@gmail.com>
2026-08-09 17:55:29 +0000

    feat(controller): add manager entrypoint
    Signed-off-by: Yashraj Shukla <shuklayashraj68@gmail.com>

---
+++ cmd/controller/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	homeostatv1alpha1 "github.com/YOUR_USERNAME/homeostat/api/v1alpha1"
+	"github.com/YOUR_USERNAME/homeostat/internal/controller"
+)
+
+var scheme = runtime.NewScheme()
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(homeostatv1alpha1.AddToScheme(scheme))
+}
+
+func main() {
+	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
+	setupLog := ctrl.Log.WithName("setup")
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{Scheme: scheme})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	if err := (&controller.HomeostatRecommendationReconciler{
+		Client: mgr.GetClient(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "HomeostatRecommendation")
+		os.Exit(1)
+	}
+
+	setupLog.Info("starting homeostat controller")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
+}
